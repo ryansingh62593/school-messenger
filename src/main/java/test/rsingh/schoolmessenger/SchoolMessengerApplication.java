74f6161135e14cb4ee7abb804ee9676963dd42ad
ryansingh62593 <ryansingh62593@gmail.com>
2022-04-30 20:32:13 -0400

    Initial commit

---
+++ src/main/java/test/rsingh/schoolmessenger/SchoolMessengerApplication.java
@@ -0,0 +1,13 @@
+package test.rsingh.schoolmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/rsingh/schoolmessenger/SchoolMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package test.rsingh.schoolmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
